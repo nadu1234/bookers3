6d01502a556841402021a8883122665247f34846
nadu1234 <146610305+nadu1234@users.noreply.github.com>
2023-10-25 07:38:23 +0000

    [Finsh]Follow function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
-    
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   def index
@@ -26,6 +27,20 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+
+
+  # フォロー一覧
+def follows
+  user = User.find(params[:id])
+  @users = user.following_user
+end
+
+# フォロワー一覧
+def followers
+  user = User.find(params[:id])
+  @user = user.follower_user
+end
+
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,29 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
+
+has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # ① フォローしている人の取得
+has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # ② フォローされているの人取得
+
+has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end	 
+
   
   
   def get_profile_image
