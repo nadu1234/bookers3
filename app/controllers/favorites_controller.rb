87be24f8941f89710aa4911fe5953d79db6c4206
nadu1234 <146610305+nadu1234@users.noreply.github.com>
2023-10-28 12:11:14 +0000

    [Start]ajax function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,10 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
       # if current_page?(controller: 'books',action: 'show')
       # redirect_to book_path(book)
       # else current_page?(books_path)
@@ -13,10 +13,10 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
       # if current_page?(controller: 'books',action: 'show')
       # redirect_to book_path(book)
       # elsif current_page?(books_path)
